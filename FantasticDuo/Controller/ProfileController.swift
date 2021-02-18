45fc4ec3da5dd25d662bc37aff0a6fb6f546006a
dev-geeyong <dev.geeyong@gmail.com>
2021-02-18 17:25:18 +0900

    add riot.txt

---
+++ FantasticDuo/Controller/ProfileController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileController.swift
+//  FantasticDuo
+//
+//  Created by dev.geeyong on 2021/02/17.
+//
+
+import UIKit
+class ProfileController: UIViewController {
+    
+    //MARK: - Propertie
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+    //MARK: - API
+    //MARK: - Actions
+    //MARK: - Helpers
+    
+}
+
